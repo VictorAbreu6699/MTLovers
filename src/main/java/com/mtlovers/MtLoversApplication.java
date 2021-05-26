db3f4d1e642a1de712acdaf69e567f56777912f7
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2021-05-26 11:33:35 -0300

    Criando o Projeto

---
+++ src/main/java/com/mtlovers/MtLoversApplication.java
@@ -0,0 +1,13 @@
+package com.mtlovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtLoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtlovers/MtLoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtlovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtLoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
